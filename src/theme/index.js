712f6b6d98974fdea4ac41c9afc81422f934af1f
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-07-11 14:51:04 +0200

    💄 Add design system and fonts

---
+++ src/theme/index.js
@@ -0,0 +1,34 @@
+const colors = {
+  black: "#000",
+  white: "#fff",
+  dark: "#00376b",
+  greyExtraLight: "#f4f4f4",
+  greyLight: "#efefef",
+  greyMedium: "#dbdbdb",
+  greySemiDark: "#737373"
+};
+
+const fonts = {
+  sizes: {
+    XS: "12px",
+    S: "14px",
+    M: "16px",
+    L: "20px"
+  },
+  weights: {
+    regular: 400,
+    medium: 500,
+    semiBold: 600
+  },
+};
+
+const borderRadius = {
+  round: "8px",
+  circle: "50%",
+};
+
+export const theme = {
+  colors,
+  fonts,
+  borderRadius,
+};
