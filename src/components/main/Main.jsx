9d1a77adc3d7bae1d9870f0b45c81226a31fe6a8
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-07-11 16:41:39 +0200

    💄 Add layout with media queries

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { styled } from "styled-components";
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  return <MainStyled>Main</MainStyled>;
+};
 
-export default Main
+export default Main;
+
+const MainStyled = styled.main`
+  background: lightblue;
+
+  height: 100vh;
+  margin-left: 244px;
+
+  @media screen and (min-width: 1920px) {
+    margin-left: 335px;
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 1264px) {
+    margin-left: 72px;
+  }
+
+  @media screen and (max-width: 767px) {
+    margin-left: 0px;
+    height: calc(100vh - 50px);
+  }
+`;
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,5 +1,37 @@
+import { styled } from "styled-components";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return <NavBarStyled>NavBar</NavBarStyled>;
 };
 
 export default NavBar;
+
+const NavBarStyled = styled.div`
+  background: #f9e7e7;
+
+  height: 100vh;
+  width: 244px;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  @media screen and (min-width: 1920px) {
+    width: 335px;
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 1264px) {
+    width: 72px;
+  }
+
+  @media screen and (max-width: 767px) {
+      bottom: 0;
+      top: auto;
+
+      height: 50px;
+      width: 100%;
+
+      z-index: 5000;
+      
+  }
+`;
