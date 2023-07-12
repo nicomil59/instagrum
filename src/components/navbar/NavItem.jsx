04b58be0ba68604bd516eeeaa699d329bec5ac92
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-07-12 16:03:03 +0200

    ♻️ NAVBAR > NavMenu > create NavItem

---
+++ src/components/navbar/NavItem.jsx
@@ -0,0 +1,52 @@
+import { styled } from "styled-components";
+import { theme } from "../../theme";
+
+const NavItem = ({ Icon, Img, text }) => {
+  return (
+    <NavItemStyled>
+      <a href="#">
+        {Icon ? Icon : Img}
+        <span className="text">{text}</span>
+      </a>
+    </NavItemStyled>
+  );
+};
+
+export default NavItem;
+
+const NavItemStyled = styled.li`
+  margin-bottom: 8px;
+
+  a {
+    padding: 12px;
+    display: flex;
+    align-items: center;
+    border-radius: 8px;
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: ${theme.colors.greyExtraLight};
+      transition: background-color 0.3s;
+
+      .icon,
+      .profile-img {
+        transform: scale(1.1);
+        transition: transform 0.3s;
+      }
+    }
+
+    .icon {
+      font-size: 1.5rem;
+    }
+
+    .icon,
+    .profile-img {
+      margin-right: 1rem;
+      transition: transform 0.3s;
+    }
+
+    .profile-img {
+      border-radius: ${theme.borderRadius.circle};
+    }
+  }
+`;
--- src/components/navbar/NavMenu.jsx
+++ src/components/navbar/NavMenu.jsx
@@ -1,60 +1,42 @@
 import { styled } from "styled-components";
-import { AiFillHome, AiOutlineCompass, AiOutlineHeart, AiOutlinePlaySquare, AiOutlinePlusSquare, AiOutlineSearch } from "react-icons/ai";
+import {
+  AiFillHome,
+  AiOutlineCompass,
+  AiOutlineHeart,
+  AiOutlinePlaySquare,
+  AiOutlinePlusSquare,
+  AiOutlineSearch,
+} from "react-icons/ai";
 import { FaRegPaperPlane } from "react-icons/fa6";
-import { theme } from "../../theme";
+import NavItem from "./NavItem";
 
 const NavMenu = () => {
   return (
     <NavMenuStyled>
       <ul>
-        <li>
-          <a href="#">
-            <AiFillHome className="icon"/>
-            <span className="text">Accueil</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <AiOutlineSearch className="icon"/>
-            <span className="text">Recherche</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <AiOutlineCompass className="icon"/>
-            <span className="text">Découvrir</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <AiOutlinePlaySquare className="icon"/>
-            <span className="text">Reels</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <FaRegPaperPlane className="icon"/>
-            <span className="text">Messages</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <AiOutlineHeart className="icon"/>
-            <span className="text">Notifications</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <AiOutlinePlusSquare className="icon"/>
-            <span className="text">Créer</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img src="https://i.pravatar.cc/24" alt="profile avatar" className="profile-img" />
-            <span className="text">Profil</span>
-          </a>
-        </li>
+        <NavItem Icon={<AiFillHome className="icon" />} text="Accueil" />
+        <NavItem Icon={<AiOutlineSearch className="icon" />} text="Recherche" />
+        <NavItem
+          Icon={<AiOutlineCompass className="icon" />}
+          text="Découvrir"
+        />
+        <NavItem Icon={<AiOutlinePlaySquare className="icon" />} text="Reels" />
+        <NavItem Icon={<FaRegPaperPlane className="icon" />} text="Messages" />
+        <NavItem
+          Icon={<AiOutlineHeart className="icon" />}
+          text="Notifications"
+        />
+        <NavItem Icon={<AiOutlinePlusSquare className="icon" />} text="Créer" />
+        <NavItem
+          Img={
+            <img
+              src="https://i.pravatar.cc/24"
+              alt="profile avatar"
+              className="profile-img"
+            />
+          }
+          text="Profil"
+        />
       </ul>
     </NavMenuStyled>
   );
@@ -67,40 +49,5 @@ const NavMenuStyled = styled.nav`
 
   ul {
     list-style-type: none;
-  }
-
-  li {
-    margin-bottom: 8px;
-  }
-
-  a {
-    padding: 12px;
-    display: flex;
-    align-items: center;
-    border-radius: 8px;
-    transition: background-color 0.3s;
-  }
-
-  a:hover {
-    background-color: ${theme.colors.greyExtraLight};
-    transition: background-color 0.3s;
-  }
-
-  .icon {
-    font-size: 1.5rem;
-  }
-  
-  .icon, .profile-img {
-    margin-right: 1rem;
-    transition: transform 0.3s;
-  }
-
-  .profile-img {
-    border-radius: ${theme.borderRadius.circle};
-  }
-  
-  a:hover .icon, a:hover .profile-img {
-    transform: scale(1.1);
-    transition: transform 0.3s;
   }
 `;
