0c9f9ef32c0f25d0aab136fdd0727c161fe19d6d
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-08-16 16:51:29 +0200

    💄 CONTENT > REELS > add content and styling

--- src/components/main/content/Reels.jsx
+++ src/components/main/content/Reels.jsx
@@ -1,7 +1,34 @@
+import { AiOutlinePlaySquare } from "react-icons/ai";
+import { styled } from "styled-components";
+import { theme } from "../../../theme";
+
 const Reels = () => {
-    return (
-      <div>Reels</div>
-    )
+  return (
+    <ReelsStyled>
+      <AiOutlinePlaySquare className="icon" />
+      <p className="tagged-text">Aucun Reel</p>
+    </ReelsStyled>
+  );
+};
+
+export default Reels;
+
+const ReelsStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px 0 60px;
+
+  .icon {
+    font-size: 62px;
+    border: 2px solid ${theme.colors.black};
+    border-radius: 50%;
+    padding: 8px;
   }
-  
-  export default Reels
+
+  .tagged-text {
+    margin-top: 24px;
+    font-size: 30px;
+    font-weight: ${theme.fonts.weights.semiBold};
+  }
+`;
