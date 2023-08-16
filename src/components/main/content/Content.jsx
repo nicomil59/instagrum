bd3b79ba6c717eaa884a5bbf9ae2688ba7cca728
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-08-16 15:44:11 +0200

    🎨 CONTENT > TABS > improve structure of tab content

--- src/components/main/content/Content.jsx
+++ src/components/main/content/Content.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { styled } from "styled-components";
 import { theme } from "../../../theme";
 import { BsGrid3X3 } from "react-icons/bs";
@@ -12,74 +12,47 @@ import Tagged from "./Tagged";
 const Content = () => {
   const [activeTab, setActiveTab] = useState("posts-tab");
 
-  // const showTab = (e) => {
-  //   console.log("click on tab");
-  //   console.log(e.target.parentNode);
-  //   setActiveTab(e.target.parentNode.id);
-  // };
+  let tabContent = "";
+
+  if (activeTab === "posts-tab") {
+    tabContent = <Posts />;
+  } else if (activeTab === "reels-tab") {
+    tabContent = <Reels />;
+  } else if (activeTab === "tagged-tab") {
+    tabContent = <Tagged />;
+  }
 
   return (
     <ContentStyled>
       <div className="tabs">
-        
-        {/* <button
-          onClick={showTab}
-          className={`tab-btn ${activeTab === "posts-tab" ? "active" : ""}`}
+        <TabItem
           id="posts-tab"
-        >
-          <BsGrid3X3 className="icon" />
-          <span className="btn-label">Publications</span>
-        </button> */}
+          label="Publications"
+          Icon={<BsGrid3X3 className="icon" />}
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+        />
 
-        <TabItem id="posts-tab" label="Publications" Icon={<BsGrid3X3 className="icon" />} activeTab={activeTab} setActiveTab={setActiveTab} />
-
-        <TabItem id="reels-tab" label="Reels" Icon={<AiOutlinePlaySquare className="icon" />} activeTab={activeTab} setActiveTab={setActiveTab} />
-
-        <TabItem id="tagged-tab" label="Tagged" Icon={<MdOutlinePersonPin className="icon" />} activeTab={activeTab} setActiveTab={setActiveTab} />
-
-        {/* <button
-          onClick={showTab}
-          className={`tab-btn ${activeTab === "reels-tab" ? "active" : ""}`}
+        <TabItem
           id="reels-tab"
-        >
-          <AiOutlinePlaySquare className="icon" />
-          <span className="btn-label">Reels</span>
-        </button> */}
+          label="Reels"
+          Icon={<AiOutlinePlaySquare className="icon" />}
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+        />
 
-        {/* <button
-          onClick={showTab}
-          className={`tab-btn ${activeTab === "tagged-tab" ? "active" : ""}`}
+        <TabItem
           id="tagged-tab"
-        >
-          <MdOutlinePersonPin className="icon" />
-          <span className="btn-label">Identifié(e)</span>
-        </button> */}
+          label="Tagged"
+          Icon={<MdOutlinePersonPin className="icon" />}
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+        />
 
       </div>
 
       <div className="content">
-        <div
-          className={`tab-content ${activeTab === "posts-tab" ? "active" : ""}`}
-          id="posts"
-        >
-          <Posts />
-        </div>
-
-        <div
-          className={`tab-content ${activeTab === "reels-tab" ? "active" : ""}`}
-          id="reels"
-        >
-          <Reels />
-        </div>
-
-        <div
-          className={`tab-content ${
-            activeTab === "tagged-tab" ? "active" : ""
-          }`}
-          id="tagged"
-        >
-          <Tagged />
-        </div>
+        {tabContent}
       </div>
     </ContentStyled>
   );
@@ -95,48 +68,11 @@ const ContentStyled = styled.section`
     justify-content: center;
     gap: 60px;
     height: 52px;
-
-    /* .tab-btn {
-      padding: 18px 0;
-      background-color: ${theme.colors.white};
-      border: 0;
-      text-align: center;
-      cursor: pointer;
-      color: ${theme.colors.greySemiDark};
-      font-weight: ${theme.fonts.weights.semiBold};
-      font-size: ${theme.fonts.sizes.XS};
-      text-transform: uppercase;
-      letter-spacing: 1px;
-
-      display: flex;
-      align-items: center;
-
-      height: 52px;
-
-      &.active {
-        color: ${theme.colors.black};
-        border-top: 1px solid ${theme.colors.black};
-
-        margin-top: -1px;
-      }
-
-      .icon {
-        margin-right: 6px;
-      }
-    } */
   }
 
   .content {
     background-color: lightblue;
     padding: 20px;
-
-    .tab-content {
-      display: none;
-
-      &.active {
-        display: block;
-      }
-    }
   }
 
   @media screen and (max-width: 735px) {
